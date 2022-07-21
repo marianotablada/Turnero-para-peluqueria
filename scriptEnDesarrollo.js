9249447d507d35230fc725ba3c1239ce39263559
Marian Tablada <mariantablada@Air-de-Marian.home>
2022-07-21 14:24:47 +0200

    v3.0

--- scriptEnDesarrollo.js
+++ scriptEnDesarrollo.js
@@ -1,109 +1,28 @@
-// CALENDARIO
-const date = new Date();
+$(document).ready( () => {
 
-const renderCalendar = () => {
-date.setDate(1);
-
-const monthsDays = document.querySelector(".days")
-
-const lastDay = new Date(date.getFullYear(),
-date.getMonth()+ 1, 0).getDate();
-
-const prevLastDay = new Date(date.getFullYear(),
-date.getMonth(), 0).getDate();
-
-const firstDayIndex = date.getDay()
-
-const lastDayIndex = new Date(date.getFullYear(),
-date.getMonth()+ 1, 0).getDay();
-
-const nextDays = 7 - lastDayIndex - 1;
-
-const months = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Deciembre",
-];
-
-document.querySelector(".date h1").innerHTML = months [date.getMonth()];
-document.querySelector(".date p").innerHTML = new Date().toDateString();
-
-let days = "";
-
-for(let x = firstDayIndex; x > 0; x--) {
-    days += `<div class= "prev-date">${prevLastDay - x + 1}</div>`;
-}
-
-for(let i= 1; i <=lastDay; i++) {
-    if(i === new Date().getDate()&&date.getMonth()=== new Date().getMonth()){
-        days += `<div class="today">${i}</div>`;    
-    }else {
-        days += `<div>${i}</div>`;
-    }
-}
-
-for(let j = 1; j <= nextDays; j++) {
-    days += `<div class= "next-date">${j}</div>`;
-    monthsDays.innerHTML = days;
-}
-}
-
-document.querySelector(`.prev`).addEventListener("click",() => {
-    date.setMonth(date.getMonth() - 1);
-        renderCalendar ();
+    $('.datepicker').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true,
+        startDate: '0d'
     });
     
-    
-    document.querySelector(`.next`).addEventListener("click",() => {
-        date.setMonth(date.getMonth() + 1);
-        renderCalendar ();
-    });
+$('.cell').click(function(){
+        $('.cell').removeClass('select');
+        $(this).addClass('select');
+    });    
+});
 
-renderCalendar ();
-
-/*
-class Persona {
-    constructor (nombre, especializacion){
-        this.nombre = nombre
-        this.especializacion = especializacion
-    }
-}
-
-const empleado1 = new Persona ("Vanesa", "tintura")
-const empleado2 = new Persona ("Brian", "corte")
-const empleado3 = new Persona ("Micaela", "maquillaje")
-const empleado4 = new Persona ("Flor", "uñas")
-*/
 
 const turnero = []
 const personas = ["Vanesa", "Brian", "Micaela", "Flor"]
 const Tiempos = [30, 20, 40, 45, 90, 60, 40]
+const Horarios = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00"]
 const servicios = ["Corte", "Peinado", "Corte y peinado", "Tintura", "Tintura y Corte"]
 
 servicios.push ("Maquillaje");
 servicios.push ("Uñas");
 
-/* const serviciosPrueba = [{id: 1, motivo: "corte", tiempo: 30},
-                {id: 2, motivo: "peinado", tiempo: 20},
-                {id: 3, motivo: "corte+peinado", tiempo: 40},
-                {id: 4, motivo: "tintura", tiempo: 45},
-                {id: 5, motivo: "tintura+corte", tiempo: 90},]
-
-serviciosPrueba.push ({id: 6, motivo: "maquillaje", tiempo: 60});
-serviciosPrueba.push ({id: 7, motivo: "uñas", tiempo: 40});
-*/
-
 //DOM´s
-
 const select = document.getElementById("select");
 
 for(let i=0; i< servicios.length; i++) {
@@ -122,64 +41,176 @@ select.addEventListener("change", () => {
 const select2 = document.getElementById("select2");
 
 for(let i=0; i< personas.length; i++) {
-    const option = document.createElement("option");
-    option.innerText = personas[i];
-    option.value = personas[i];
-    select2.append(option);
+    const option = document.createElement("option"); // qué tipo de TAG quiero agregar
+    option.innerText = personas[i]; // qué datos voy a agregar
+    option.value = personas[i]; // qué valor va a tener
+    select2.append(option); // dondé lo va a agregar?
 }
 
-const botonAceptar = document.getElementById("botonAceptar");
-const botonFiltrar = document.getElementById("botonFiltrar");
 const botonContinuar = document.getElementById("botonContinuar");
+const botonFinalizar = document.getElementById("botonFinalizar");
+const botonAceptar = document.getElementById("botonAceptar");
 const tituloPrincipal = document.getElementById("brand");
 const botonAgendarTurno = document.getElementById("buttonAgendar")
-const mensajeFinal = document.getElementById("mensajeFinal");
-const inputNombre = document.getElementById("inputNombre");
-const imagen = document.getElementsByClassName("imagen_page_1");
+const botonConsultarTurno = document.getElementById("buttonConsultaTurno")
+let mensajeFinal = document.getElementById("mensajeFinal");
+let inputNombre = document.getElementById("inputNombre");
+// const imagen = document.getElementById("imagen_page_1");
 const formulario1 = document.getElementById("formulario1");
 const seccionPrincipal = document.getElementById("main_firstSeccion");
 const seccionCalendario = document.getElementById("main_therdSection");
+let checks = document.querySelectorAll(`.valores`);
+let myBtn = document.getElementById("myBtn");
+let menuHorarios = document.getElementsByClassName("cell");
+
+/*
+console.log(menuHorarios[0].innerText);
+
+menuHorarios.addEventListener("click", () => {
+    console.log(menuHorarios.indexOf);
+    let resultadoHorarioSeleccionado = menuHorarios[indexOf];
+    console.log(resultadoHorarioSeleccionado);
+});
+
+menuHorarios.addEventListener("click", () => {
+    tween9.play();
+});
+*/
+
+function chequearFormularioEnviado (){
+    const formularioEnviado = localStorage.getItem("turnoRegistrado");
+    // swal("Bienvenido/a !");
+    if (formularioEnviado !== null){
+        console.log("SI se registró ningún turno desde este dispositivo");
+        // tween6.play();    
+    }else{
+        console.log("Aún NO se registró ningún turno desde este dispositivo");
+        botonConsultarTurno.style.display = "none";
+    }
+}
+chequearFormularioEnviado ();
 
 //EVENTOS
 
 botonAgendarTurno.addEventListener("click", () =>  {
-    //alert("Se hizo click sobre el boton Agendar Turno");
-    botonAgendarTurno.style.display = "none";
-    formulario1.style.display = "flex";
-    seccionPrincipal.style.height = 700 + `px`;
-    
+        // botonAgendarTurno.style.visibility = "hidden";
+        tweenButtonAgendar.play();
+        tweenButtonConsultaTurno.play();
+        seccionPrincipal.style.height = 240 + `px`;
+        tween.play(); //esto están en el scriptGSAP
+        tween2.play(); //esto están en el scriptGSAP
+        tween3.play(); //esto están en el scriptGSAP
+        tween5.play();
+       // botonConsultarTurno.style.visibility = "hidden";
 });
-//Opción 2-
-/* botonAgendarTurno.onclikn = () => {
+
+botonConsultarTurno.addEventListener("click", () => {
+    swal({
+        text: 'Ingrese el número de su turno',
+        content: "input", // cero que aquí debería ir number
+        button: {
+          text: "Buscar!",
+          closeModal: false,
+        },
+      })
+      .then(name => {
+        if (!name) throw null;
+        return fetch(`https://itunes.apple.com/search?term=${name}&entity=movie`);
+      })
+      .then(results => {
+        return results.json();
+      })
+      .then(json => {
+        const movie = json.results[0];
+        if (!movie) {
+          return swal("No tenemos un turno registrado con ese número, por favor, comunicate con nuestro atención al cliente para ayudarte");
+        }
+        const name = movie.trackName;
+        const imageURL = movie.artworkUrl100;
+        swal({
+          title: "Top result:",
+          text: name,
+          icon: imageURL,
+        });
+      })
+      .catch(err => {
+        if (err) {
+          swal("Oh noes!", "The AJAX request failed!", "error");
+        } else {
+          swal.stopLoading();
+          swal.close();
+        }
+      });
+});
+
+inputNombre.addEventListener("change", () => {
+    tween8.play();
+});
+
+/*Opción 2-
+    botonAgendarTurno.onclikn = () => {
     alert("Se hizo click sobre el boton Agendar Turno");
 }
 */
 
-// captar en un EVENT la opción seleccionada en el select
+// let horarioSeleccionado = document.querySelector(".cell py-1 select");
+// 
 
+// captar en un EVENT la opción seleccionada en el select
 // SESION STORAGE
 
-botonAceptar.addEventListener("click", () => {
+botonContinuar.addEventListener("click", () => {
+    // main_secondSection.style.display = "none";
     console.log(inputNombre.value);
     console.log(select.value);
     console.log(servicios.indexOf(select.value));
     console.log(Tiempos[servicios.indexOf(select.value)]);
     console.log(select2.value);
+    // formulario1.style.visibility = "hidden";
     seccionCalendario.style.display = "inline-block";
-
     sessionStorage.setItem("nombre ingresado", (inputNombre.value));
     sessionStorage.setItem("servicio seleccionado", (select.value));
     sessionStorage.setItem("profesional seleccionado", (select2.value));
-
 });
 
-botonContinuar.addEventListener("click", () => {
+
+
+botonContinuar.addEventListener("click",function(){
+    checks.forEach((e) => {
+        if(e.checked == true){
+            console.log(e.value); 
+            swal(`Te vamos a esperar con un rico ${e.value} !`);
+        };
+    });
+});
+
+/* Desarrollo de botón para volver atrás al formulario y corregir algún 
+
+myBtn.addEventListener("click", () => {
+    alert("se apretó el boton volver atrás");
+    tween9.reverse();
+    tween6.reverse();
+    tween8.reverse();
+    tweenButtonAgendar.reverse();
+    tweenButtonConsultaTurno.reverse();
+});
+*/
+
+let inputFecha = document.getElementById("dp1")
+
+
+botonAceptar.addEventListener("click", () => {
+
+    let horarioSeleccionado = document.querySelector(".cell.select");
+    
+    console.log(horarioSeleccionado.innerText);
+
 
     const turno = {
         nombre: inputNombre.value,
         motivo: select.value,
-        // día:
-        // hora:
+        día: inputFecha.value,
+        hora: horarioSeleccionado.innerText,
         profesional: select2.value,
         tiempo: Tiempos[servicios.indexOf(select.value)],
     };
@@ -190,22 +221,34 @@ botonContinuar.addEventListener("click", () => {
     mensajeFinalNombreCliente.innerText =   `Muchas gracias ${inputNombre.value}!
                                             Tienes un turno para: ${select.value}
                                             Con: ${select2.value}
-                                            El día: 
-                                            A las: `;
+                                            El: ${inputFecha.value} 
+                                            A las: ${horarioSeleccionado.innerText}`;
     mensajeFinalNombreCliente.className = "tituloFinal";
     mensajeFinal.append(mensajeFinalNombreCliente);
 
     turnero.push(turno);
     
     console.log(turnero);
-    localStorage.setItem("turnoRegistrado", 1);
-    
-    //JSON
+    // myBtn.style.display = "none";
 
+
+localStorage.setItem("turnoRegistrado", parseInt(Math.random()));
+
+    //JSON
+    /*
     const enJSON = JSON.stringify(turno);
     localStorage.setItem("turno", enJSON);
     
     // localStorage.setItem("turno", turno);
+    */
+    seccionCalendario.style.display = "none";
+
+    swal({
+        title: `Muchas gracias ${inputNombre.value}!`,
+        text: `Se registró tu turno CORRECTAMENTE`,
+        icon: "success",
+        button: "Finalizar",
+    });
 });
 
 //EVENTOS -Sumit
@@ -214,105 +257,32 @@ formulario1.addEventListener("submit", (event) => {
     event.preventDefault();
 });
 
-/* 
-const formulario2 = document.getElementById("formulario2");
 
-formulario2.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
-*/
-
-
-gsap.to(`.brand`, {
-    duration: 1,
-    y: -55,
-    delay: 2,
-    fontSize: `100px`,
-});
-
-gsap.to(`.brand2`, {
-    duration: 1,
-    y: -55,
-    delay: 2,
+botonFinalizar.addEventListener("click", () => {
+    for(let i=0; i< personas.length; i++) {
+        const turnosFiltrados = turnero.filter((turno) => {
+            return turno.profesional.includes(personas[i]);
+        });
+        const sumall = turnosFiltrados.map(item => item.tiempo).reduce((prev, curr) => prev + curr, 0);
+            console.log(sumall);
+        if(sumall < 480){
+            console.log(i);
+        }else{
+            select2.remove(i);
+            // console.log(personas[i]);
+        };
+    };
 });
 
-gsap.to(`.imagen_page_1`, {
-    y: 55,
-    duration: 1,
-    delay: 2,
-    display: `none`,
-
-}); 
-
-/*
-botonFiltrar.addEventListener("click", () => {
-    
-    const nombreDelProfesionalElegido = select2.value;
-    
-    const turnosFiltrados = turnero.filter((turno) => {
-    
-        return turno.profesional.includes(nombreDelProfesionalElegido);
-    });
-
-    console.log(turnosFiltrados);
-
-});
-*/ 
-
-//El botón filtro lo agregué para probar haber si funcionaba el código para la entrega final no va a estar
-// Con este código genero 4 nuevos arrays con los turnos filtrado por "profesionales" del array Turnero, y luego hago la suma de los tiempos de cada servicio
-// Este es el código que voy a utilizar más adelante para limitar la cantidad de turnos que se van a poder generar
-botonFiltrar.addEventListener("click", () => {
-const turnosFiltrados = turnero.filter((turno) => {
-    return turno.profesional.includes("Vanesa");
-});
-console.log(turnosFiltrados);
-const sumall = turnosFiltrados.map(item => item.tiempo).reduce((prev, curr) => prev + curr, 0);
-console.log(sumall);
-
-});
-
-botonFiltrar.addEventListener("click", () => {
-    const turnosFiltrados = turnero.filter((turno) => {
-    return turno.profesional.includes("Brian");
-    }); 
-    const sumall = turnosFiltrados.map(item => item.tiempo).reduce((prev, curr) => prev + curr, 0);
-    console.log(sumall);
-});
-
-botonFiltrar.addEventListener("click", () => {
-    const turnosFiltrados = turnero.filter((turno) => {
-    return turno.profesional.includes("Flor");
-    });
-    const sumall = turnosFiltrados.map(item => item.tiempo).reduce((prev, curr) => prev + curr, 0);
-console.log(sumall);
-});
-
-botonFiltrar.addEventListener("click", () => {
-    const turnosFiltrados = turnero.filter((turno) => {
-        return turno.profesional.includes("Micaela");
-    });
-    const sumall = turnosFiltrados.map(item => item.tiempo).reduce((prev, curr) => prev + curr, 0);
-console.log(sumall);
-// + sesionStorage
+/* + de sesionStorage
 for (let i = 0; i < sessionStorage.length; i++){
     const clave = sessionStorage.key (i);
     const valor = sessionStorage.getItem(clave);
     console.log(`Para la clave: ${clave}, el valor es: ${valor} `);
 }
 });
-
-function chequearFormularioEnviado (){
-    const formularioEnviado = localStorage.getItem("turnoRegistrado");
-    if (formularioEnviado !== null){
-        console.log("Se registró un turno desde este dispositivo");
-    }else{
-        console.log("Aún no se registró ningún turno desde este dispositivo");
-    }
-}
-
-chequearFormularioEnviado();
-/* Con el siguiente código había eliminado datos del localStorage que cargue mientras practicaba
-
-localStorage.removeItem("turnos");
 */
+
+// Local Storage 
+// localStorage.removeItem("turnos");
+// localStorage.clear();
---
+++ scriptGSAP.js
@@ -0,0 +1,62 @@
+// trabajar con Tween todas las variables deben contener la propiedad "paused: true", sino cuando se cargue la página se va a ejecutar el comando
+let tween = gsap.to(`.brand`, {
+    duration: 1,
+    y: -55,
+    delay: 0,
+    fontSize: `100px`,
+    paused: true
+})
+
+let tween2 = gsap.to(`.brand2`, {
+    duration: 1,
+    y: -55,
+    delay: 0,
+    paused: true
+});
+
+//Opacity es para hacer esconder (aunque solo se ocula de la vista, porque sigue ocupando el mismo espacio) el elemento
+let tween3 = gsap.to(`.imagen_page_1`, {
+    duration: 0.5, 
+    //x: 100, 
+    y: -50, 
+    opacity: 0,
+    fontSize: `100px`,
+    paused: true
+    
+});
+
+let tweenButtonAgendar = gsap.to(`#buttonAgendar`, {
+    duration: 0,  
+    opacity: 0,
+    delay: 0,
+    paused: true
+});
+
+let tweenButtonConsultaTurno = gsap.to(`#buttonConsultaTurno`, {
+    duration: 0,
+    delay: 0,  
+    opacity: 0,
+    paused: true
+});
+
+//autoAlpha hace que aparezca el elemento. Es importante verificar aquí que el elemento en el CSS tenga la propiedad "visibility: hidden,"
+let tween5 = gsap.to(`#formulario1`, {
+    duration: 2, 
+    autoAlpha: 1,
+    delay: 0.5, 
+    paused: true
+});
+
+let tween8 = gsap.to(`.bn633-hover`, { //botón Continuar 
+    duration: 3,
+    autoAlpha: 1,
+    delay: 0.5,
+    paused: true
+});
+
+let tween9 = gsap.to(`.bn634-hover`, { //botón Aceptar
+    duration: 2, 
+    autoAlpha: 1,
+    delay: 0.5, 
+    paused: true
+});
